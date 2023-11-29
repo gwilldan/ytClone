9e8c606ce24c423339619b901b6c1e2de676de1a
gwilldan <sayhi2godswill@gmail.com>
2023-11-29 18:16:52 +0100

    styling components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,12 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
+import { Header } from "./components";
 
-function App() {
-	const [searchInput, setSearchInput] = useState("");
-	const [search, setSearch] = useState("");
-
-	useEffect(() => {
-		console.log("SEARCH", search);
-	}, [search]);
-
+const App = () => {
 	return (
-		<div className=" p-10 grid place-content-center h-screen w-screen">
-			<div className=" w-[1000px] h-[800px] ">
-				<div className=" flex justify-center items-center">
-					<input
-						type="text"
-						className=" border border-black h-10 w-[400px] px-3"
-						onChange={(e) => setSearchInput(e.target.value)}
-					/>
-					<button
-						className=" bg-slate-800 text-white h-10 px-3"
-						onClick={() => setSearch(searchInput)}>
-						SEARCH
-					</button>
-				</div>
-			</div>
-		</div>
+		<main>
+			<Header />
+		</main>
 	);
-}
+};
 
 export default App;
---
+++ src/utils/fetchData.jsx
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+const options = {
+	// URL ://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=funny%20cat&key=[YOUR_API_KEY]
+	// https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=funny%20cat%20FACTS&key=[YOUR_API_KEY]
+};
